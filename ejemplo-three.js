a3d8e121c8269e3c7694fb96d5a5e97c8660335b
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-02-21 13:53:25 -0600

    Update ejemplo-three.js

--- ejemplo-three.js
+++ ejemplo-three.js
@@ -1,6 +1,6 @@
 var escena = new THREE.Scene();
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 5
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerHeight*.9, window.innerHeight*.9);
 document.body.appendChild( renderer.domElement );
