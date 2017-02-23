6b2938625922679732de7c777ba5a3ed275e5fd9
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-02-23 14:06:18 -0600

    Update Lata2.js

--- Lata2.js
+++ Lata2.js
@@ -14,7 +14,7 @@ var escena = new THREE.Scene();
 escena.add(rMalla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 300;
+camara.position.z = 15;
 
 renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
