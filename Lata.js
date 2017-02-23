7929d1971f3e7b00a10284dd6c880afc977ec769
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-02-23 13:54:43 -0600

    Update Lata.js

--- Lata.js
+++ Lata.js
@@ -1,6 +1,6 @@
 var lataForma = new THREE.CylinderGeometry(1, 1, 2);
 var tapaForma = new THREE.CylinderGeometry(0.75, 1, 0.2);
-tapaForma.translate(0,2,0);
+tapaForma.translate(0,1,0);
 
 var lataMalla = new THREE.Mesh(lataForma);
 var tapaMalla = new THREE.Mesh(tapaForma);
