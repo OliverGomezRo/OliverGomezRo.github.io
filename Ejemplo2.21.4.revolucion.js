13aeced43bd442ebb104fdf3e673a913fed84e9f
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-02-28 14:31:32 -0600

    Update Ejemplo2.21.4.revolucion.js

--- Ejemplo2.21.4.revolucion.js
+++ Ejemplo2.21.4.revolucion.js
@@ -1,19 +1,11 @@
-var puntos = [];
-
-for ( var i = 0; i < 50; i ++ ) {
-    puntos.push( new THREE.Vector2(
-                     Math.sin( i * 0.2 ) * 15 + 50,
-                     ( i - 5 ) * 2 ) );
+var points = [];
+for ( var i = 0; i < 10; i ++ ) {
+	points.push( new THREE.Vector2( Math.sin( i * 0.2 ) * 10 + 5, ( i - 5 ) * 2 ) );
 }
-var forma = new THREE.LatheGeometry(puntos);
-
-var material = new THREE.MeshNormalMaterial();
-
-var malla = new THREE.Mesh( forma, material );
-malla.rotateX( Math.PI/6 );
-var escena = new THREE.Scene();
-escena.add(malla);
-
+var geometry = new THREE.LatheGeometry( points );
+var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+var lathe = new THREE.Mesh( geometry, material );
+scene.add( lathe );
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 500;
 
