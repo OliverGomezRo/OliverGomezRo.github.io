1b943ef3658bde4bd6e145335ef6b9c373891414
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-02-28 13:43:36 -0600

    Update Lata3.js

--- Lata3.js
+++ Lata3.js
@@ -1,6 +1,6 @@
 var troncoForma = new THREE.CylinderGeometry(5, 5, 10);
 var esferaForma = new THREE.CylinderGeometry(4,5,3);
-esferaForma.translate(0,10,0);
+esferaForma.translate(0,7.5,0);
 
 var troncoMalla = new THREE.Mesh(troncoForma);
 var esferaMalla = new THREE.Mesh(esferaForma);
@@ -14,7 +14,7 @@ var escena = new THREE.Scene();
 escena.add(arbolMalla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 15
+camara.position.z = 50;
 camara.position.y = 10;
 
 renderizador = new THREE.WebGLRenderer();
