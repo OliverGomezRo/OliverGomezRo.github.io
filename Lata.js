10cfe7317dc122d1c6617b3277cec8546f652af3
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-02-28 13:20:36 -0600

    Update Lata.js

--- Lata.js
+++ Lata.js
@@ -2,6 +2,11 @@ var lataForma = new THREE.CylinderGeometry(3, 3, 10);
 var tapaForma = new THREE.CylinderGeometry(2, 3, 0.5);
 tapaForma.translate(0,10,0);
 
+var geometry = new THREE.CylinderGeometry( 5, 5, 20, 32 );
+var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+var cylinder = new THREE.Mesh( geometry, material );
+scene.add( cylinder );
+
 var lataMalla = new THREE.Mesh(lataForma);
 var tapaMalla = new THREE.Mesh(tapaForma);
 var rForma = new THREE.Geometry();
@@ -14,9 +19,7 @@ var escena = new THREE.Scene();
 escena.add(rMalla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 40;
-camara.position.y = 20;
-camara.position.x = 30;
+
 
 renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
