ef8eb4273e9e5e7abe3c5dd5ee20b667e95ea92b
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-02-28 14:27:43 -0600

    Update Resistor.js

--- Resistor.js
+++ Resistor.js
@@ -2,7 +2,7 @@ var puntos = [];
 
 for ( var i = 0; i < 50; i ++ ) {
     puntos.push( new THREE.Vector2(
-                     Math.sin( i * 0.5 ) * 15 + 50,
+                     Math.sin( i * 1 ) * 15 + 50,
                      ( i - 5 ) * 2 ) );
 }
 var forma = new THREE.LatheGeometry(puntos);
@@ -10,7 +10,7 @@ var forma = new THREE.LatheGeometry(puntos);
 var material = new THREE.MeshNormalMaterial();
 
 var malla = new THREE.Mesh( forma, material );
-malla.rotateX( Math.PI/4 );
+malla.rotateX( Math.PI/6);
 var escena = new THREE.Scene();
 escena.add(malla);
 
