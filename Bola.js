6638c81fedd093f28d2ffc4fdc55b81302d4dd59
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-03-04 16:22:23 -0600

    Update Bola.js

--- Bola.js
+++ Bola.js
@@ -5,6 +5,18 @@ for ( var i = 0; i < 50; i ++ ) {
                      Math.sin( i * 0.2 ) * 10 + 20,
                      ( i - 5 ) * 3 ) );
 }
+
+var pataForma = new THREE.CylinderGeometry(1, 1, 1);
+esferaForma.translate(0,-26,0);
+
+var pataMalla = new THREE.Mesh(pataForma);
+var arbolForma = new THREE.Geometry();
+pForma.merge(pataMalla.geometry, pataMalla.matrix);
+var material = new THREE.MeshNormalMaterial();
+var pMalla = new THREE.Mesh(pForma, material);
+var escena = new THREE.Scene();
+escena.add(arbolMalla);
+
 var forma = new THREE.LatheGeometry(puntos);
 
 var material = new THREE.MeshNormalMaterial();
