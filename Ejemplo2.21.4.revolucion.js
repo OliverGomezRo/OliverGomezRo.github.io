55b5c3f1aa6a3faaebfe277f0ba3314bdbfd8115
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-03-04 17:27:30 -0600

    Update Ejemplo2.21.4.revolucion.js

--- Ejemplo2.21.4.revolucion.js
+++ Ejemplo2.21.4.revolucion.js
@@ -1,6 +1,8 @@
 var points = [];
-for ( var i = 0; i < 10; i ++ ) {
-	points.push( new THREE.Vector2( Math.sin( i * 0.2 ) * 10 + 5, ( i - 5 ) * 2 ) );
+for ( var i = 0; i < 50; i ++ ) {
+    puntos.push( new THREE.Vector2(
+                     Math.sin( i * 0.2 ) * 15 + 50,
+                     ( i - 5 ) * 2 ) );
 }
 var geometry = new THREE.LatheGeometry( points );
 var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
