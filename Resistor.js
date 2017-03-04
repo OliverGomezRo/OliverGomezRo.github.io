9a3adce271f2938e988e7a2cc82ea73854695149
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-03-04 17:12:15 -0600

    Update Resistor.js

--- Resistor.js
+++ Resistor.js
@@ -3,6 +3,10 @@ var puntos = [];
 for ( var i = 0; i < 10; i ++ ) {
     puntos.push( new THREE.Vector2(i*2 , i*10);
 }
+    var geometry = new THREE.CubeGeometry(20, 20, 20); // Create a 20 by 20 by 20 cube.
+    var material = new THREE.MeshBasicMaterial({ color: 0x0000FF }); // Skin the cube with 100% blue.
+    var cube = new THREE.Mesh(geometry, material); // Create a mesh based on the specified geometry (cube) and material (blue skin).
+  
 var forma = new THREE.LatheGeometry(puntos);
 
 var material = new THREE.MeshNormalMaterial();
@@ -11,7 +15,7 @@ var malla = new THREE.Mesh( forma, material );
 malla.rotateX( Math.PI/6);
 var escena = new THREE.Scene();
 escena.add(malla);
-
+  escena.add(cube);
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 50;
 
