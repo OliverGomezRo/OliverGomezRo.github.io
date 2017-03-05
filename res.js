55538ec198abbd5b3e0111b98ff7483fae3f75b4
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-03-05 15:44:44 -0600

    Update res.js

--- res.js
+++ res.js
@@ -4,7 +4,14 @@ for ( var i = 0; i < 50; i ++ ) {
                      Math.sin( i * 0.2 ) * 10 + 20,
                      ( i - 5 ) * 3 ) );
 }
+var esferaForma = new THREE.SphereGeometry(.65);
+esferaForma.translate(0,1,0);
 
+var troncoMalla = new THREE.Mesh(troncoForma);
+var arbolForma = new THREE.Geometry();
+arbolForma.merge(troncoMalla.geometry, troncoMalla.matrix);
+
+var arbolMalla = new THREE.Mesh(arbolForma, material);
 
 var forma = new THREE.LatheGeometry(puntos);
 
@@ -14,6 +21,7 @@ var malla = new THREE.Mesh( forma, material );
 malla.rotateY( Math.PI/6);
 var escena = new THREE.Scene();
 escena.add(malla);
+escena.add(arbolMalla);
 
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 500;
