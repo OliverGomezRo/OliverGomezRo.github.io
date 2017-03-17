dfb0fa6c594694dc4ea0189fa1237af1c9b5120b
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-03-17 00:03:01 -0600

    Rename Practica2.js to Practica3.js

---
+++ Practica3.js
@@ -0,0 +1,32 @@
+var campoVision = 45;
+var relacionAspecto = window.innerWidth/ window.innerHeight;
+var planoCercano = 1;
+var planoLejano = 1000;
+var mesh ;
+var i=0;
+var j=0;
+
+var camara= new THREE.PerspectiveCamera(campoVision, relacionAspecto, planoCercano, planoLejano);
+camara.position.z = 25;
+camara.position.y = 5;
+camara.position.x = 3.5;
+
+var escena= new THREE.Scene();
+
+for (i=0;i<8;i++)
+{
+	for(j=0;j<8;j++)
+	{
+	mesh = new THREE.Mesh(new THREE.BoxGeometry(1,0.1,1));
+	mesh.position.x=i;
+	mesh.position.z=j;	
+	//mesh.rotateX(Math.PI/6);
+	escena.add(mesh);
+	}
+}
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena, camara);
