178f77f5e6e95e62b4813087f28db1e2a6e78526
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-05-09 13:54:34 -0500

    Update Texturas.js

--- Texturas.js
+++ Texturas.js
@@ -1,6 +1,6 @@
 function setup(){
 THREE.ImageUtils.crossOrigin = '';
-var textura = new THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/crate.gif');
+var textura = new THREE.ImageUtils.loadTexture('https://olivergomezro.github.io/crate.gif');
 var material = new THREE.MeshBasicMaterial( {map: textura} );
 var forma = new THREE.BoxGeometry(1,1,1);
 malla = new THREE.Mesh(forma,material);
