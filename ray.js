064408c0472616cf6a6ef2f783b2296a6c6c1584
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-05-23 14:26:45 -0500

    Create ray.js

---
+++ ray.js
@@ -0,0 +1,44 @@
+function setup(){
+  cubo1=new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial());
+  cubo2=new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial());
+  pelota=new THREE.Mesh(new THREE.SphereGeometry(0.5), new THREE.MeshNormalMaterial());
+  cubo1.position.x=0.7;
+  cubo2.position.x=-0.7;
+  
+  camara=new THREE.PerspectiveCamera();
+  camara.position.z=5;
+  
+  raycaster1 = new THREE.Raycaster(pelota.position, new THREE.Vector3(1,0,0));
+  raycaster2 = new THREE.Raycaster(pelota.position, new THREE.Vector3(-1,0,0));
+
+  escena=new THREE.Scene();
+  escena.add(cubo1);
+  escena.add(cubo2);
+  escena.add(camara);
+  escena.add(pelota);
+  
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerHeight*0.95,window.innerHeight*0.95);
+  
+  document.body.appendChild(renderer.domElement);
+  step=0.01;
+}
+function loop(){
+  obstaculo1 = raycaster.intersectObjects(cubo1);
+  obstaculo2 = raycaster.intersectObjects(cubo2);
+  if (obstaculo1.length >0 && (obstaculo1[0].distance <=0.5)) || 
+  (obstaculo2.length >0 && (obstaculo2[0].distance <=0.5)))
+  step=-step;
+  
+  pelota position.x += step;
+  rayacaster1.set(pelota.position, new THREE.Vector3(1,0,0));
+  rayacaster2.set(pelota.position, new THREE.Vector3(1,0,0));
+  
+  renderer.render(escena,camara);
+  requestAnimationFrame(loop); 
+}
+var cubo1, cubo2, escena, camara,renderer, pelota;
+var raycaster1,raycaster2, step
+var obstaculo1, obstaculo2;
+setup();
+loop();
