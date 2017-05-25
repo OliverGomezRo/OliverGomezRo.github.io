8f76e55237ee9067e04a3ff7ead8c52a940b54a8
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-05-25 14:14:12 -0500

    Update ray.js

--- ray.js
+++ ray.js
@@ -2,11 +2,11 @@ function setup(){
   cubo1=new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial());
   cubo2=new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial());
   pelota=new THREE.Mesh(new THREE.SphereGeometry(0.5), new THREE.MeshNormalMaterial());
-  cubo1.position.x=0.7;
-  cubo2.position.x=-0.7;
+  cubo1.position.x=5;
+  cubo2.position.x=-5;
   
   camara=new THREE.PerspectiveCamera();
-  camara.position.z=5;
+  camara.position.z=20;
   
   raycaster1 = new THREE.Raycaster(pelota.position, new THREE.Vector3(1,0,0));
   raycaster2 = new THREE.Raycaster(pelota.position, new THREE.Vector3(-1,0,0));
@@ -14,8 +14,9 @@ function setup(){
   escena=new THREE.Scene();
   escena.add(cubo1);
   escena.add(cubo2);
-  escena.add(camara);
   escena.add(pelota);
+  escena.add(camara);
+
   
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerHeight*0.95,window.innerHeight*0.95);
