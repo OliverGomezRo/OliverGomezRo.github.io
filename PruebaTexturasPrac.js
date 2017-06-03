ef369200a6c0af0bb309a22b0736cafdc7af8dc8
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-06-03 16:40:17 -0500

    Update PruebaTexturasPrac.js

--- PruebaTexturasPrac.js
+++ PruebaTexturasPrac.js
@@ -1,6 +1,6 @@
 function setup(){
 THREE.ImageUtils.crossOrigin = '';
-var textura = new THREE.TextureLoader('https://olivergomezro.github.io/board_empty.gif');
+var textura = new THREE.ImageUtils.loadTexture('https://olivergomezro.github.io/crate.gif');
 var material = new THREE.MeshBasicMaterial( {map: textura} );
 var forma = new THREE.BoxGeometry(1,0.1,1);
 
