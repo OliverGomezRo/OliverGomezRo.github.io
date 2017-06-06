3defc02d74f5b03047038260c2e243261a283574
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-06-06 14:10:32 -0500

    Update Agent.js

--- Agent.js
+++ Agent.js
@@ -32,3 +32,68 @@ Environment.prototype.act = funciton(){
       this.children[i].act(this);
   }
 }
+
+function Pelota(r,x=0,y=0){
+  Agent.call(this,x,y);
+  this.add(new THREE.Mesh(new THREE.SphereGeometry(r), new THREE.MeshNormalMaterial()));
+  this.step=0.1;
+  this.colision=0;
+  this.radio=r;
+  this.sensor=new THREE.Raycaster(this.position,new THREE.vector3(1,0,0));
+}
+Pelota.prototype=new Agent();
+
+Pelota.prototype.sense = function(environment){
+  this.sensor.set(this.position, new THREE.Vector3(1,0,0));
+  var obstaculo1 = this.sensor.intersectObjects(environment.children,true);
+  
+  this.sensor.set(this.position, new THREE.Vector3(-1,0,0));
+  var obstaculo2 = this.sensor.intersectObjects(environment.children,true);
+  if((obsctaculo1.length > 0 && (obstaculo1[0].distance <= this.radio)) || 
+     (obsctaculo2.length > 0 && (obstaculo2[0].distance <= this.radio)))
+     this.colision=1;
+  else
+     this.colision=0;
+};//Se pone punto y coma cuando es una asignación
+Pelota.prototype.act = function(environment){
+  if(this.colision === 1)
+     this.step = -this.step;
+  this.position.x += this.step;
+};//Se pone punto y coma cuando es una asignación
+function Pared(size,x=0,y=0){
+  THREE.Object3D.call(this,x,y);
+  this.add(new THREE.Mesh(new THREE.BoxGeometry(size,size,size),new THREE.MeshNormalMaterial()));
+  this.size=size;
+  this.position.x=x;
+  this.position.y=y;
+}
+Pared.prototype=new THREE.Object3D();
+
+function setup(){
+  entorno = new Environment();
+  camara = new THREE.PerspectiveCamera();
+  camara.position.z=30;
+  
+  entorno.add(new Pared(1,7,0));
+  entorno.add(new Pared(1,-7,0));
+  entorno.add(new Pared(1,7,1));
+  entorno.add(new Pared(1,-7,1));
+  entorno.add(new Pared(1,7,-1));
+  entorno.add(new Pared(1,-7,-1));
+  entorno.add(new Pelota(0.5));
+  entorno.add(camara);
+  
+  render = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerHeight*0.95,window.innerHeight*0.95);
+  document.body.appendChild(renderer.domElement);
+}
+function loop(){
+  requestAnimationFrame(loop);
+  entorno.sense();
+  entorno.plan();
+  entorno.act();
+  renderer.render(entorno,camara);  
+}
+var entorno,camara,renderer;
+setup();
+loop();
