8d728dcbb4df94bb86f59ba05403c77ddfacf659
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-06-07 00:38:52 -0500

    Update ray.js

--- ray.js
+++ ray.js
@@ -22,7 +22,7 @@ function setup(){
   renderer.setSize(window.innerHeight*0.95,window.innerHeight*0.95);
   
   document.body.appendChild(renderer.domElement);
-  step=1;
+  step=0.1;
 }
 function loop(){
   obstaculo1 = raycaster1.intersectObject(cubo1);
