91a41c6e00cdf426450436226baca76d55722d99
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-06-06 22:36:36 -0500

    Update PruebaTexturasPrac.js

--- PruebaTexturasPrac.js
+++ PruebaTexturasPrac.js
@@ -20,7 +20,7 @@ for (i=0;i<8;i++)
 camara = new THREE.PerspectiveCamera();
 camara.position.z=25;
 camara.position.y=5;
-camara.position.x=0;
+camara.position.x=2;
 
 renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
