0b5b7ccb6a45de94283d68379e62b819277222d7
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-06-08 13:09:13 -0500

    Create app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 function setup(){
 THREE.ImageUtils.crossOrigin = '';
 var textura = new THREE.ImageUtils.loadTexture('https://olivergomezro.github.io/concrete.png');
-var material = new THREE.MeshBasicMaterial( {map: textura} );
+var material = new THREE.MeshPhongMaterial( {map: textura} );
 var forma = new THREE.BoxGeometry(1,0.1,1);
 var texturaPelota = new THREE.ImageUtils.loadTexture('https://olivergomezro.github.io/Pelota.png');	
 var pelota= THREE.Mesh(new THREE.SphereGeometry(1),new THREE.MeshPhongMaterial({map: texturaPelota}));
 escena = new THREE.Scene();
-
+	
+pelota.position.set(4, 10, 4);
 light= new THREE.PointLight(0xffffff);
-light.position.set(1, 1, 1.3);
+light.position.set(4, 10, 4);
 escena.add(light);
 	
 for (i=0;i<8;i++)
@@ -23,9 +24,9 @@ for (i=0;i<8;i++)
 }
 
 camara = new THREE.PerspectiveCamera();
-camara.position.z=25;
-camara.position.y=5;
-camara.position.x=2;
+camara.position.z=4;
+camara.position.y=6;
+camara.position.x=4;
 
 renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
