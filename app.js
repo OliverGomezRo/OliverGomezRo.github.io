9892491d0d160e1349b1b7fe7b7bef0b00274ad8
OliverGomezRo <oliver.go.ro@hotmail.com>
2017-06-10 23:16:15 -0500

    Create app.js

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
 function setup(){
-	
 THREE.ImageUtils.crossOrigin = '';
-	
 var textura = new THREE.ImageUtils.loadTexture('https://olivergomezro.github.io/concrete.png');
-var material = new THREE.MeshBasicMaterial( );
+var material = new THREE.MeshBasicMaterial( {map: textura} );
 var forma = new THREE.BoxGeometry(1,0.1,1);
-var texturaPelota = new THREE.ImageUtils.loadTexture('https://olivergomezro.github.io/Pelota.png');	
-var pelota= new THREE.Mesh(new THREE.SphereGeometry(1),new THREE.MeshBasicMaterial());
-	
+
 escena = new THREE.Scene();
-	
-pelota.position.x=4;
-pelota.position.y=1;
-pelota.position.z=4;
-	
-var iluminacion = new THREE.PointLight(0xFFFFFF);
-iluminacion.position.y=20;
-escena.add(iluminacion);
-		
+
 for (i=0;i<8;i++)
 {
 	for(j=0;j<8;j++)
@@ -28,19 +16,15 @@ for (i=0;i<8;i++)
 	escena.add(malla);
 	}
 }
-//60, window.innerWidth/ window.innerHeight, 1, 1000
-var camara= new THREE.PerspectiveCamera();
-camara.position.z=4;
-camara.position.y=6;
-camara.position.x=4;
-	
-escena.add(pelota);
-//camara.lookAt(pelota.position);
-	
+
+camara = new THREE.PerspectiveCamera();
+camara.position.z=25;
+camara.position.y=5;
+camara.position.x=2;
+
 renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth*.95,window.innerHeight*.95);
+renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderer.domElement);
-	
 }
 
 function loop(){
@@ -50,6 +34,6 @@ requestAnimationFrame(loop);
 
 renderer.render(escena,camara);
 }
-var camara, escena, renderer, malla, pelota;
+var camara, escena, renderer, malla;
 setup();
 loop();
